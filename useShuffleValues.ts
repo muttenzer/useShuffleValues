827479c978d0193da8a0bf76270399d7c864ae29
Timo <49474412+muttenzer@users.noreply.github.com>
2023-08-07 18:30:03 +0200

    Update useShuffleValues.ts

--- useShuffleValues.ts
+++ useShuffleValues.ts
@@ -5,11 +5,8 @@ export type GenericArrayWithMinimumTwoItems<T> = [T, T, ...T[]];
 export const useShuffleValues = <T,>(
     values: GenericArrayWithMinimumTwoItems<T>
 ) => {
-    const getRandomValue = (arr?: GenericArrayWithMinimumTwoItems<T>) => {
-        if (arr) {
-            return arr[Math.floor(Math.random() * values.length)];
-        }
-        return values[Math.floor(Math.random() * values.length)];
+    const getRandomValue = (arr: ValueType<T> = values) => {
+        return arr[Math.floor(Math.random() * arr.length)];
     };
 
     const [currentValue, setCurrentValue] = useState(getRandomValue());
